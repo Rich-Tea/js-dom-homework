ca8162048d0a6ba93dec8dd2699ad5ae2e9b4a37
Rich <richgrant192@gmail.com>
2020-03-29 14:30:05 +0100

    added html list

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const newItemForm = document.querySelector('#game-form');
     newItemForm.addEventListener('submit', newItemFormSubmit);
 })
+
+const newItemFormSubmit = function (e) {
+    event.preventDefault();
+
+const gameListItem = createGameListItem(e.target);
+const gameList = document.querySelector('#game-list');
+gameList.appendChild(gameListItem);
+
+e.target.reset();
+}

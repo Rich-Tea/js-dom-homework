db03a5c0ed7647186377d0e847063334fed26d80
Rich <richgrant192@gmail.com>
2020-03-29 16:37:05 +0100

    created list elements in document

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const newItemForm = document.querySelector('#game-form');
     newItemForm.addEventListener('submit', newItemFormSubmit);
 })
 
-const newItemFormSubmit = function (e) {
+const newItemFormSubmit = function (event) {
     event.preventDefault();
 
-const gameListItem = createGameListItem(e.target);
+const gameListItem = createGameListItem(event.target);
 const gameList = document.querySelector('#game-list');
 gameList.appendChild(gameListItem);
 
-e.target.reset();
+
 }
+
+const createGameListItem = function (form) {
+    const gameListItem = document.createElement('li');
+    gameList.classList.add('game-list-item');
+
+    const title = document.createElement('h2');
+    title.textContent = form.title.value;
+    gameListItem.appendChild(title);
+  
+    const genre = document.createElement('h3');
+    title.textContent = form.genre.value;
+    gameListItem.appendChild(genre);
+
+    const platform = document.createElement('p');
+    title.textContent = form.platform.value;
+    gameListItem.appendChild(platform);
+    
+ return gameListItem;
+
+}
+
+

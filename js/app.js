f2a0c736bffd5ae27d98e7cc66e402ba73903495
Rich <richgrant192@gmail.com>
2020-03-29 14:25:49 +0100

    submit event object added

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newItemForm = document.querySelector('#game-form');
+    newItemForm.addEventListener('submit', newItemFormSubmit);
+})
